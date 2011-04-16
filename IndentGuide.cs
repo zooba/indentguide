4efb8509ea5b478b51e0c2f52e2a07f57f603733
Steve Dower <steve@stevedower.id.au>
2011-04-16 21:42:51 +1000

    Added guides on blank lines.

--- IndentGuide.cs
+++ IndentGuide.cs
@@ -1,13 +1,10 @@
-using System.Windows;
-using System.Windows.Controls;
+using System;
 using System.Windows.Media;
 using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Formatting;
-using System.ComponentModel.Composition;
-using Microsoft.VisualStudio.Editor;
-using System;
-using Microsoft.VisualStudio.Text.Classification;
+using System.Collections.Generic;
 
 namespace IndentGuide
 {
@@ -16,8 +13,6 @@ namespace IndentGuide
         IAdornmentLayer Layer;
         IWpfTextView View;
         Brush GuideBrush;
-
-        private static readonly Color LineColor = Colors.LightGray;
 
         public IndentGuide(IWpfTextView view, IEditorFormatMapService formatMapService)
         {
@@ -50,44 +45,82 @@ namespace IndentGuide
         {
             int tabSize = View.Options.GetOptionValue(DefaultOptions.TabSizeOptionId);
 
-            foreach (ITextViewLine line in e.NewOrReformattedLines)
+            foreach (ITextViewLine _line in e.NewOrReformattedLines)
             {
+                var line = _line;
                 var snapshot = line.Snapshot;
-                int actualPos = 0;
-                int spaceCount = tabSize;
-                int end = line.End;
-                for (int i = line.Start; i <= end; ++i)
+                if (line.Length == 0)
                 {
-                    char c = i == end ? ' ' : snapshot[i];
+                    int lineNumber = line.Snapshot.GetLineNumberFromPosition(line.Start.Position);
+                    if (lineNumber > snapshot.LineCount) continue;
 
-                    if (actualPos > 0 && (actualPos % tabSize) == 0 && char.IsWhiteSpace(c) &&
-                        snapshot.Length > i)
+                    for (int i = lineNumber - 1;
+                        i >= 0;
+                        i -= 1)
                     {
-                        var span = new SnapshotSpan(snapshot, i, 1);
-                        var marker = View.TextViewLines.GetMarkerGeometry(span);
-                        var guide = new System.Windows.Shapes.Line()
+                        var line2 = snapshot.GetLineFromLineNumber(i);
+                        if (line2.Length > 0)
                         {
-                            X1 = marker.Bounds.Left,
-                            Y1 = marker.Bounds.Top,
-                            X2 = marker.Bounds.Left,
-                            Y2 = marker.Bounds.Bottom,
-                            Stroke = GuideBrush,
-                            StrokeDashArray = new DoubleCollection { 10.0, 5.0 },
-                            StrokeThickness = 0.25,
-                            SnapsToDevicePixels = true
-                        };
-
-                        Layer.AddAdornment(span, null, guide);
+                            try
+                            {
+                                line = View.TextViewLines.GetTextViewLineContainingBufferPosition(line2.Start);
+                            }
+                            catch (ArgumentOutOfRangeException)
+                            { }
+                            break;
+                        }
                     }
+                    if (line == null) continue;
+                }
 
-                    if (c == '\t')
-                        actualPos = ((actualPos / tabSize) + 1) * tabSize;
-                    else if (c == ' ')
-                        actualPos += 1;
-                    else
-                        break;
+                if (line.Length > 0)
+                {
+                    int actualPos = 0;
+                    int spaceCount = tabSize;
+                    int end = line.End;
+                    for (int i = line.Start; i <= end; ++i)
+                    {
+                        char c = i == end ? ' ' : snapshot[i];
+
+                        if (actualPos > 0 && (actualPos % tabSize) == 0 && char.IsWhiteSpace(c) &&
+                            snapshot.Length > i)
+                        {
+                            double left = GetGuideLeft(snapshot, i);
+                            AddGuide(_line, left);
+                        }
+
+                        if (c == '\t')
+                            actualPos = ((actualPos / tabSize) + 1) * tabSize;
+                        else if (c == ' ')
+                            actualPos += 1;
+                        else
+                            break;
+                    }
                 }
             }
+        }
+
+        private double GetGuideLeft(ITextSnapshot snapshot, int i)
+        {
+            var span = new SnapshotSpan(snapshot, i, 1);
+            var marker = View.TextViewLines.GetMarkerGeometry(span);
+            return marker.Bounds.Left;
+        }
+        
+        private void AddGuide(ITextViewLine line, double left)
+        {
+            var guide = new System.Windows.Shapes.Line()
+            {
+                X1 = left,
+                Y1 = line.Top,
+                X2 = left,
+                Y2 = line.Bottom,
+                Stroke = GuideBrush,
+                StrokeThickness = 1.0,
+                SnapsToDevicePixels = true
+            };
+
+            Layer.AddAdornment(new SnapshotSpan(line.Start, line.End), null, guide);
         }
     }
 }
