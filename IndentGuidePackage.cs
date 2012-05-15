761ee68344fff3a8a3d40ca1d6ad7cb76f8cb835
Steve Dower <steve@stevedower.id.au>
2012-05-15 16:54:43 +1000

    26: Edit.ViewIndentGuides cannot be executed from the command window Improved command processing and updates.

--- IndentGuidePackage.cs
+++ IndentGuidePackage.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace IndentGuide {
     [PackageRegistration(UseManagedResourcesOnly = true)]
@@ -38,14 +37,11 @@ namespace IndentGuide {
         private int cmdidViewIndentGuides = cmdidViewIndentGuides2010;
 
         private EnvDTE.WindowEvents WindowEvents;
-        private EnvDTE.DTEEvents DTEEvents;
         private IIndentGuide Service;
+        private bool CommandVisible;
 
         protected override void Initialize() {
             base.Initialize();
-
-            // Adding the command is deferred to reduce VS start-up time.
-            _menuCommand = null;
 
             // Prepare event
             var dte = GetService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
@@ -54,17 +50,25 @@ namespace IndentGuide {
                     cmdidViewIndentGuides = cmdidViewIndentGuides11;
                 }
 
+                CommandVisible = false;
                 WindowEvents = dte.Events.WindowEvents;
                 WindowEvents.WindowActivated +=
                     new EnvDTE._dispWindowEvents_WindowActivatedEventHandler(WindowEvents_WindowActivated);
-                DTEEvents = dte.Events.DTEEvents;
-                DTEEvents.OnBeginShutdown +=
-                    new EnvDTE._dispDTEEvents_OnBeginShutdownEventHandler(DTEEvents_OnBeginShutdown);
+            }
+
+            // Add our command handlers for menu (commands must exist in the .vsct file)
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (mcs != null) {
+                // Create the command for the tool window
+                CommandID viewIndentCommandID = new CommandID(guidIndentGuideCmdSet, cmdidViewIndentGuides);
+                var menuCmd = new OleMenuCommand(ToggleVisibility, viewIndentCommandID);
+                menuCmd.BeforeQueryStatus += new EventHandler(BeforeQueryStatus);
+
+                mcs.AddCommand(menuCmd);
             }
 
             // Assume the service exists, otherwise, crash the extension.
             Service = (IIndentGuide)GetService(typeof(SIndentGuide));
-            Service.VisibleChanged += new EventHandler(Service_VisibleChanged);
         }
 
         /// <summary>
@@ -75,53 +79,20 @@ namespace IndentGuide {
                 Service.Save();
         }
 
-        private MenuCommand _menuCommand;
-        /// <summary>
-        /// Lazily initialises and returns the menu item for showing
-        /// and hiding indent guides.
-        /// </summary>
-        /// <returns>An instance of <see cref="MenuCommand"/> or
-        /// <b>null</b>.</returns>
-        MenuCommand GetCommand() {
-            if (_menuCommand != null) return _menuCommand;
-
-            // Add our command handlers for menu (commands must exist in the .vsct file)
-            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-
-            if (mcs != null) {
-                // Create the command for the tool window
-                CommandID viewIndentCommandID = new CommandID(guidIndentGuideCmdSet, cmdidViewIndentGuides);
-                var menuCmd = new MenuCommand(ToggleVisibility, viewIndentCommandID);
-                menuCmd.Enabled = true;
-                menuCmd.Checked = Service.Visible;
-
-                if (System.Threading.Interlocked.CompareExchange(ref _menuCommand, menuCmd, null) == null) {
-                    mcs.AddCommand(_menuCommand);
-                    return _menuCommand;
-                }
-            }
-
-            return null;
+        void WindowEvents_WindowActivated(EnvDTE.Window GotFocus, EnvDTE.Window LostFocus) {
+            CommandVisible = (GotFocus != null && GotFocus.Kind == "Document");
         }
 
-        void WindowEvents_WindowActivated(EnvDTE.Window GotFocus, EnvDTE.Window LostFocus) {
-            var menuCmd = GetCommand();
-            if (menuCmd == null) return;
+        void BeforeQueryStatus(object sender, EventArgs e) {
+            var item = (OleMenuCommand)sender;
 
-            menuCmd.Visible = (GotFocus != null && GotFocus.Kind == "Document");
+            item.Enabled = true;
+            item.Checked = Service.Visible;
+            item.Visible = CommandVisible;
         }
 
         private void ToggleVisibility(object sender, EventArgs e) {
             Service.Visible = !Service.Visible;
-        }
-
-        void Service_VisibleChanged(object sender, EventArgs e) {
-            var service = sender as IIndentGuide;
-            if (service == null) return;
-            var menuCmd = GetCommand();
-            if (menuCmd == null) return;
-
-            menuCmd.Checked = service.Visible;
         }
 
         // Default version is 10.9.0.0, also known as 11 (beta 1).
