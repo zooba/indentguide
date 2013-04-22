ec74575be34d89d54c79623d38288c031b360dda
Steve Dower <steve@stevedower.id.au>
2013-04-21 22:40:14 -0700

    v13 (beta 3... again...): Fixed page width guide location (again...)

--- Guides/IndentGuide.cs
+++ Guides/IndentGuide.cs
@@ -192,20 +192,19 @@ namespace IndentGuide {
             var caret = CaretHandlerBase.FromName(Theme.CaretHandler, View.Caret.Position.VirtualBufferPosition, Analysis.TabSize);
 
             foreach (var line in analysisLines.Concat(GetPageWidthLines())) {
-                double top, bottom, left;
+                double top, bottom;
+                double left = line.Indent * spaceWidth + horizontalOffset;
 
                 if (line.Type == LineSpanType.PageWidthMarker) {
                     line.Highlight = (Analysis.LongestLine > line.Indent);
 
                     top = View.ViewportTop;
                     bottom = View.ViewportBottom;
-                    left = (line.Indent - 1) * spaceWidth + horizontalOffset;
                 } else {
                     caret.AddLine(line, willUpdateImmediately: true);
 
                     top = View.TextViewLines.FirstVisibleLine.Top;
                     bottom = View.TextViewLines.LastVisibleLine.Bottom;
-                    left = line.Indent * spaceWidth + horizontalOffset;
                 }
 
                 if (line.FirstLine >= 0 && line.LastLine < int.MaxValue) {
