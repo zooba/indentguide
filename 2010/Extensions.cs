48a9d966b3730f3b09f1fdac653ff89bd26a3efd
Steve Dower <steve@stevedower.id.au>
2013-07-11 16:34:54 -0700

    Adds VS 2010 support as a separate project.

---
+++ 2010/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Windows.Threading;
+
+namespace IndentGuide {
+    static class Extensions {
+        public static object GetValue(this PropertyInfo prop, object obj) {
+            return prop.GetValue(obj, null);
+        }
+
+        public static void SetValue(this PropertyInfo prop, object obj, object value) {
+            prop.SetValue(obj, value, null);
+        }
+
+        public static T GetCustomAttribute<T>(this ICustomAttributeProvider provider) {
+            return provider.GetCustomAttributes(false).OfType<T>().FirstOrDefault();
+        }
+
+        public static void InvokeAsync(this Dispatcher dispatcher, Action action) {
+            dispatcher.BeginInvoke(action);
+        }
+    }
+}
