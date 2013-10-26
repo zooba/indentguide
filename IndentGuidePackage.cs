3480579f96ceaf4ced625e6f4f63fdec1aab5e43
Steve Dower <steve@stevedower.id.au>
2013-10-26 14:14:57 -0700

    v14: Upped version.

--- IndentGuidePackage.cs
+++ IndentGuidePackage.cs
@@ -23,7 +23,7 @@ using Microsoft.VisualStudio.Shell;
 
 namespace IndentGuide {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "14 (Beta 2)", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", "14", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(DisplayOptions), "IndentGuide", "Display", 110, 120, false)]
     [ProvideOptionPage(typeof(BehaviorOptions), "IndentGuide", "Behavior\\QuickSet", 110, 130, false)]
