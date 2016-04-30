44efd0f81c3e9ac1be1855b6b5b398c8276e814c
Steve Dower <steve@stevedower.id.au>
2016-04-30 14:49:24 -0700

    Adds test file folder and moves files.

---
+++ TestFiles/Issue73.cs
@@ -0,0 +1,36 @@
+namespace Demo
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+            // Comment
+
+            for (int pos = 0; pos <= 10; pos++)
+            {
+                char ch = ' ';
+                char next = ' ';
+            }
+        }
+    }
+}
